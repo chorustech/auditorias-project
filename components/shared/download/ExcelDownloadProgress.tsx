52e5e182346b376586b257720737767ee691f91a
garibcatastrofe <ramsesgaribflorescuen@gmail.com>
2026-03-14 20:38:15 -0700

    feat: :sparkles: Cambio de estilo a componente DownloadProgress

--- components/shared/download/ExcelDownloadProgress.tsx
+++ components/shared/download/ExcelDownloadProgress.tsx
@@ -9,20 +9,11 @@ export default function DownloadProgress() {
 
   return (
     <div
-      className={`
-        fixed top-6 right-6
-        bg-black text-white
-        px-4 py-2 rounded
-        shadow-lg
-        transition-all duration-500
-        ${progress === 100 ? "translate-x-40 opacity-0" : ""}
-      `}
+      className={`fixed top-4 right-4 bg-white px-4 py-2 rounded-lg border border-neutral-200 transition-all duration-500 z-80 ${progress === 100 ? "translate-x-40 opacity-0" : ""}`}
     >
-      <div className="text-sm mb-1">Generando Excel</div>
+      <div className="text-sm mb-1">Generando Excel...</div>
 
-      <p>{progress}</p>
-
-      <div className="w-48 h-2 bg-gray-700 rounded">
+      <div className="w-48 h-2 bg-green-600 rounded">
         <div
           className="h-2 bg-green-400 rounded transition-all"
           style={{ width: `${progress}%` }}
